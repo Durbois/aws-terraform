644399caecc66caf7d66c79598d1208c60e89ba1
Hadrien Taghuo <hadrien.taghuo@ti8m.ch>
2023-09-04 06:47:44 +0200

    add ips tags

--- ec2-instances/main.tf
+++ ec2-instances/main.tf
@@ -10,3 +10,21 @@ resource "aws_internet_gateway" "gateway" {
 
   tags = var.tags
 }
+
+
+resource "aws_route_table" "route-table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example.id
+  }
+
+  tags = var.tags
+}
+
+resource "aws_subnet" "public-subnet" {
+  cidr_block = "172.16.0.16/28"
+  vpc_id     = aws_vpc.main.id
+}
+
--- ec2-instances/variables.tf
+++ ec2-instances/variables.tf
@@ -5,3 +5,15 @@ variable "tags" {
         Environment = "dev"        
     }
 }
+
+variable "ips" {
+    type = map
+    default = {
+        "vpc" = "172.16.0.0/24",
+        "public-subnet-a" = "172.16.0.16/28",
+        "public-subnet-b" = "172.16.0.32/28",
+        "private-subnet-a" = "172.16.0.48/28",
+        "private-subnet-b" = "172.16.0.64/28",
+
+    }
+}
