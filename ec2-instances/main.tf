8c9455e6fb6143bc4b7013018f0d92011bd0a075
Hadrien Taghuo <hadrien.taghuo@ti8m.ch>
2023-09-06 06:22:31 +0200

    files tf formatted

--- ec2-instances/main.tf
+++ ec2-instances/main.tf
@@ -1,14 +1,14 @@
 resource "aws_vpc" "main" {
   cidr_block = var.ips["vpc"]
 
-  tags       = var.tags
+  tags = var.tags
 }
 
 
 resource "aws_internet_gateway" "gateway" {
   vpc_id = aws_vpc.main.id
 
-  tags   = var.tags
+  tags = var.tags
 }
 
 
@@ -20,7 +20,7 @@ resource "aws_route_table" "route_table" {
     gateway_id = aws_internet_gateway.gateway.id
   }
 
-  tags         = var.tags
+  tags = var.tags
 }
 
 resource "aws_subnet" "public_subnet" {
@@ -34,11 +34,11 @@ resource "aws_route_table_association" "public_association" {
 }
 
 data "aws_key_pair" "ec2_key" {
-  key_name = "ec2-key"
+  key_name           = "ec2-key"
   include_public_key = true
 
   filter {
-    name = "key-pair-id"
+    name   = "key-pair-id"
     values = ["key-0d05f3162c1bf0576"]
   }
 }
@@ -47,11 +47,11 @@ resource "aws_instance" "public_ec2" {
   ami                    = var.amis["linux_23"]
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet.id
-  key_name               = data.aws_key_pair.ec2_key.id 
+  key_name               = data.aws_key_pair.ec2_key.id
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-  
-  
-  tags            = var.tags
+
+
+  tags = var.tags
 }
 
 resource "aws_security_group" "allow_ssh" {
@@ -60,11 +60,11 @@ resource "aws_security_group" "allow_ssh" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = var.tags
--- ec2-instances/variables.tf
+++ ec2-instances/variables.tf
@@ -1,27 +1,27 @@
 variable "tags" {
-    type = map(string)
-    default = {
-        Name = "main"
-        Environment = "dev"        
-    }
+  type = map(string)
+  default = {
+    Name        = "main"
+    Environment = "dev"
+  }
 }
 
 variable "ips" {
-    type = map(string)
-    default = {
-        "vpc" = "172.16.0.0/24",
-        "public_subnet_a" = "172.16.0.16/28",
-        "public_subnet_b" = "172.16.0.32/28",
-        "private_subnet_a" = "172.16.0.48/28",
-        "private_subnet_b" = "172.16.0.64/28",
+  type = map(string)
+  default = {
+    "vpc"              = "172.16.0.0/24",
+    "public_subnet_a"  = "172.16.0.16/28",
+    "public_subnet_b"  = "172.16.0.32/28",
+    "private_subnet_a" = "172.16.0.48/28",
+    "private_subnet_b" = "172.16.0.64/28",
 
-    }
+  }
 }
 
 variable "amis" {
-    type = map(string)
-    default = {
-      "linux_23" = "ami-051f7e7f6c2f40dc1"
-    }
-  
+  type = map(string)
+  default = {
+    "linux_23" = "ami-051f7e7f6c2f40dc1"
+  }
+
 }
