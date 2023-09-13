51797bd65837478c45960678cf74ca75a135981e
Hadrien Taghuo <hadrien.taghuo@ti8m.ch>
2023-09-13 08:31:46 +0200

    remove vpc sedurity group

--- ec2-instances/main.tf
+++ ec2-instances/main.tf
@@ -97,7 +97,6 @@ resource "aws_launch_template" "template" {
   image_id = data.aws_ami.amz_ami.id
   instance_type = "t2.micro"
   key_name = data.aws_key_pair.ec2_key.key_name
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   network_interfaces {
     associate_public_ip_address = true
