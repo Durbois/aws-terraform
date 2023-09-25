28e6081c73b76ac9b91dda91f1ba84da0d1c0ed5
Hadrien Taghuo <hadrien.taghuo@ti8m.ch>
2023-09-25 06:49:09 +0200

    sg from lb added

--- ec2-instances/main.tf
+++ ec2-instances/main.tf
@@ -81,6 +81,14 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTP from sg lb"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    security_groups = [ aws_security_group.sg_lb.id ]
+  }
+
   tags = var.tags
 }
 
