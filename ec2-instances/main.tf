2b527b7ad31959a3dc35c5fe5c9e091fc12d6d26
Hadrien Taghuo <hadrien.taghuo@ti8m.ch>
2023-09-28 05:30:04 +0200

    add egress

--- ec2-instances/main.tf
+++ ec2-instances/main.tf
@@ -87,13 +87,13 @@ resource "aws_security_group" "allow_traffic" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-  # ingress {
-  #   description = "HTTP from sg lb"
-  #   from_port = 0
-  #   to_port = 65355
-  #   protocol = "tcp"
-  #   security_groups = [ aws_security_group.sg_lb.id ]
-  # }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
   tags = var.tags
 }
