5ae2c169c2342fb73adcf2fede3d785d1f5b0850
Hadrien Taghuo <hadrien.taghuo@ti8m.ch>
2023-10-11 05:53:23 +0200

    comment sg

--- ec2-instances/main.tf
+++ ec2-instances/main.tf
@@ -81,35 +81,35 @@ resource "aws_security_group" "allow_traffic" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   from_port        = 80
+  #   to_port          = 80
+  #   protocol         = "tcp"
+  #   cidr_blocks      = ["0.0.0.0/0"]
+  # }
 
-   ingress {
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    # security_groups  = [aws_security_group.sg_lb.id]
-    cidr_blocks      = ["0.0.0.0/0"]
-  } 
+  #  ingress {
+  #   from_port        = 8080
+  #   to_port          = 8080
+  #   protocol         = "tcp"
+  #   # security_groups  = [aws_security_group.sg_lb.id]
+  #   cidr_blocks      = ["0.0.0.0/0"]
+  # } 
 
-   ingress {
-    from_port        = 8081
-    to_port          = 8081
-    protocol         = "tcp"
-    # security_groups  = [aws_security_group.sg_lb.id]
-    cidr_blocks      = ["0.0.0.0/0"]
-  } 
+  #  ingress {
+  #   from_port        = 8081
+  #   to_port          = 8081
+  #   protocol         = "tcp"
+  #   # security_groups  = [aws_security_group.sg_lb.id]
+  #   cidr_blocks      = ["0.0.0.0/0"]
+  # } 
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+  # egress {
+  #   from_port        = 0
+  #   to_port          = 0
+  #   protocol         = "-1"
+  #   cidr_blocks      = ["0.0.0.0/0"]
+  # }
 
   tags = var.tags
 }
