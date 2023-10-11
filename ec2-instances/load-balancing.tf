1dc76130b5dfabf9c3877ebf7e07b5c2e3fe23c0
Hadrien Taghuo <hadrien.taghuo@ti8m.ch>
2023-10-11 07:07:56 +0200

    add target

--- ec2-instances/load-balancing.tf
+++ ec2-instances/load-balancing.tf
@@ -102,17 +102,19 @@ resource "aws_lb_target_group" "target" {
   port = 80
   protocol = "HTTP"
   vpc_id = aws_vpc.main.id
+  target_type = "instance"
+  ip_address_type = "ipv4"
   
-  # health_check {
-  #   enabled             = true
-  #   port                = 8081
-  #   interval            = 30
-  #   protocol            = "HTTP"
-  #   path                = "/health"
-  #   matcher             = "200"
-  #   healthy_threshold   = 3
-  #   unhealthy_threshold = 3
-  # }
+  health_check {
+    enabled             = true
+    port                = "traffic-port"
+    interval            = 30
+    protocol            = "HTTP"
+    path                = "/health"
+    matcher             = "200-399"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
 
 }
 
