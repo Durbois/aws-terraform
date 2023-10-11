5681d71ab9ee4974b4e9209dff2b65a4b90aeee9
Hadrien Taghuo <hadrien.taghuo@ti8m.ch>
2023-10-11 07:21:53 +0200

    uncomment ingress and egress

--- ec2-instances/main.tf
+++ ec2-instances/main.tf
@@ -81,12 +81,12 @@ resource "aws_security_group" "allow_traffic" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # ingress {
-  #   from_port        = 80
-  #   to_port          = 80
-  #   protocol         = "tcp"
-  #   cidr_blocks      = ["0.0.0.0/0"]
-  # }
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
   #  ingress {
   #   from_port        = 8080
@@ -104,12 +104,12 @@ resource "aws_security_group" "allow_traffic" {
   #   cidr_blocks      = ["0.0.0.0/0"]
   # } 
 
-  # egress {
-  #   from_port        = 0
-  #   to_port          = 0
-  #   protocol         = "-1"
-  #   cidr_blocks      = ["0.0.0.0/0"]
-  # }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
   tags = var.tags
 }
