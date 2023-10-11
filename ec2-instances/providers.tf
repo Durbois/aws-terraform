17bcdd6bc5e080d7ab08e31a76a2e093176fc7fe
Hadrien Taghuo <hadrien.taghuo@ti8m.ch>
2023-10-11 06:48:47 +0200

    providers changed

--- ec2-instances/providers.tf
+++ ec2-instances/providers.tf
@@ -9,5 +9,8 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "eu-central-1"
+#  region = "eu-central-1"
+  shared_config_files      = ["/Users/tah/.aws/config"]
+  shared_credentials_files = ["/Users/tah/.aws/credentials"]
+  profile                  = "default"  
 }
