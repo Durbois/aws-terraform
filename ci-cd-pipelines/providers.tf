a0d40c1e242fe431e5de8200cc93ed2f9d57594a
Hadrien Taghuo <hadrien.taghuo@ti8m.ch>
2023-11-29 06:08:08 +0100

    relocate providers

--- ci-cd-pipelines/providers.tf
+++ ci-cd-pipelines/providers.tf
@@ -10,12 +10,24 @@ terraform {
 }
 
 provider "aws" {
-  region = var.region
+  region = local.region
   default_tags {
     tags = {
       Owner       = var.owner
       Project     = var.project
       Environment = var.environment
     }
+  }
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
 }
