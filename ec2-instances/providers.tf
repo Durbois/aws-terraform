6416d96d33ac7cc45dbd39a13d1777c1dae03d17
Hadrien Taghuo <hadrien.taghuo@ti8m.ch>
2024-02-03 07:46:30 +0100

    revert changes

--- ec2-instances/providers.tf
+++ ec2-instances/providers.tf
@@ -1,17 +1,16 @@
 terraform {
-  required_version = ">= 1.3"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.34"
+      version = "5.15.0"
     }
   }
 }
+
 # Configure the AWS Provider
-# provider "aws" {
-# #  region = "eu-central-1"
-#   shared_config_files      = ["/Users/tah/.aws/config"]
-#   shared_credentials_files = ["/Users/tah/.aws/credentials"]
-#   profile                  = "default"  
-# }
+provider "aws" {
+#  region = "eu-central-1"
+  shared_config_files      = ["/Users/tah/.aws/config"]
+  shared_credentials_files = ["/Users/tah/.aws/credentials"]
+  profile                  = "default"  
+}
