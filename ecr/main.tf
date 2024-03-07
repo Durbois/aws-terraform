0404b98719f23561350af0d6a2a2e537d1b2cb42
Hadrien Taghuo <hadrien.taghuo@ti8m.ch>
2024-03-07 06:31:05 +0100

    typo string fixed

--- ecr/main.tf
+++ ecr/main.tf
@@ -31,7 +31,7 @@ module "ecr" {
   repository_name = local.name
 
   repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
-  repository_image_tag_mutability = MUTABLE
+  repository_image_tag_mutability = "MUTABLE"
   create_lifecycle_policy           = true
   repository_lifecycle_policy = jsonencode({
     rules = [
