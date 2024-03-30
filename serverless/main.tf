77def8fbcefc5025dd3c778a5706be4328db2a19
Hadrien Taghuo <hadrien.taghuo@ti8m.ch>
2024-03-30 08:07:12 +0100

    add lambda document

---
+++ serverless/main.tf
@@ -0,0 +1,45 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = {
+      Hashicorp-Learn = "aws-iam-policy"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "document" {
+  statement {
+    actions   = [   "dynamodb:DeleteItem",
+                    "dynamodb:GetItem",
+                    "dynamodb:PutItem",
+                    "dynamodb:Query",
+                    "dynamodb:Scan",
+                    "dynamodb:UpdateItem"
+                ]
+    resources = ["*"]
+    effect = "Allow"
+  }
+  statement {
+    actions   = [   "logs:CreateLogGroup",
+                    "logs:CreateLogStream",
+                    "logs:PutLogEvents"
+                ]
+    resources = ["*"]
+    effect = "Allow"
+  }
+}
+
+
+
+resource "aws_iam_policy" "policy" {
+  name        = "lambda-apigateway-policy"
+  description = "Lambda policy"
+
+  policy = data.aws_iam_policy_document.document.json
+
+  
+}
