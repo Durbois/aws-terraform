acaca86cd3073966deb6321cf105f6d577715885
Hadrien Taghuo <hadrien.taghuo@ti8m.ch>
2024-04-04 07:21:03 +0200

    add role and attach policy

--- serverless/main.tf
+++ serverless/main.tf
@@ -35,11 +35,32 @@ data "aws_iam_policy_document" "document" {
 
 
 
-resource "aws_iam_policy" "policy" {
+resource "aws_iam_policy" "lambda_policy" {
   name        = "lambda-apigateway-policy"
   description = "Lambda policy"
 
   policy = data.aws_iam_policy_document.document.json
+ 
+}
 
-  
+data "aws_iam_policy_document" "instance_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda-apigateway-role"
+  assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
+
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
 }
