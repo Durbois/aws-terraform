67112b05ca700209b87b543563d1ccea8b7a0b00
Hadrien Taghuo <hadrien.taghuo@ti8m.ch>
2024-04-15 21:38:40 +0200

    integrate lambda function

--- serverless/main.tf
+++ serverless/main.tf
@@ -136,3 +136,12 @@ resource "aws_api_gateway_method" "rest_method" {
   resource_id   = aws_api_gateway_resource.rest_resource.id
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
 }
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
+  resource_id             = aws_api_gateway_resource.rest_resource.id
+  http_method             = aws_api_gateway_method.rest_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.terraform_lambda_func.invoke_arn
+}
