618dde406fe40bd59524e14b75e750aa67b73774
Hadrien Taghuo <hadrien.taghuo@ti8m.ch>
2024-04-16 06:47:12 +0200

    change integration type

--- serverless/main.tf
+++ serverless/main.tf
@@ -142,6 +142,6 @@ resource "aws_api_gateway_integration" "integration" {
   resource_id             = aws_api_gateway_resource.rest_resource.id
   http_method             = aws_api_gateway_method.rest_method.http_method
   integration_http_method = "POST"
-  type                    = "AWS_PROXY"
+  type                    = "AWS"
   uri                     = aws_lambda_function.terraform_lambda_func.invoke_arn
 }
