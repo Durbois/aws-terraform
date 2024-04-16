f3ab0e7b402c6f0f7324961cc26ea4247aa746eb
Hadrien Taghuo <hadrien.taghuo@ti8m.ch>
2024-04-16 07:19:20 +0200

    set apigateway permission

--- serverless/main.tf
+++ serverless/main.tf
@@ -137,6 +137,17 @@ resource "aws_api_gateway_method" "rest_method" {
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
 }
 
+# Lambda
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.terraform_lambda_func.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.rest_api.id}/*/${aws_api_gateway_method.rest_method.http_method}${aws_api_gateway_resource.rest_resource.path}"
+}
+
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = aws_api_gateway_rest_api.rest_api.id
   resource_id             = aws_api_gateway_resource.rest_resource.id
@@ -144,4 +155,6 @@ resource "aws_api_gateway_integration" "integration" {
   integration_http_method = "POST"
   type                    = "AWS"
   uri                     = aws_lambda_function.terraform_lambda_func.invoke_arn
+
+  depends_on                     = [aws_lambda_function.terraform_lambda_func]
 }
--- serverless/variables.tf
+++ serverless/variables.tf
@@ -3,3 +3,13 @@ variable "table_name" {
     type = string
     default = "lambda-apigateway"  
 }
+
+variable "region" {
+    type = string
+    default = "eu-central-1"
+}
+
+variable "account_id" {
+    type = string
+    default = "419755718183"
+}
